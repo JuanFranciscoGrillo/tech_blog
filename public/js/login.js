6e37933474d3dd1afdaa40ffb2d7dc23928157f1
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-11 18:15:24 -0400

    Added front-end JavaScript files for login, registration, and post interactions in public/js

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+// Handling user login
+document.querySelector('#login-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // Redirect to dashboard or homepage after successful login
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+});

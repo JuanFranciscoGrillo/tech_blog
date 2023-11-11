fa93db682deccc6c38d2e7d4f737489073049998
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-11 17:04:43 -0400

    Created User model with password encryption and validation

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    // Check password method
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [8] }
+        }
+    },
+    {
+        hooks: {
+            beforeCreate: async (userData) => {
+                userData.password = await bcrypt.hash(userData.password, 10);
+                return userData;
+            }
+        },
+        sequelize,
+        timestamps: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;

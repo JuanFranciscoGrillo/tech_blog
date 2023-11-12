cfc164d5af776f5e866dc7d924caa5b9d185088d
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-12 12:19:38 -0400

    Expand script.js to include general event handling templates

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // More event listeners as needed
 });
+document.addEventListener("DOMContentLoaded", () => {
+    // Example: Form submission handling
+    const forms = document.querySelectorAll("form");
+    forms.forEach(form => {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            // Handle form submission logic here
+            console.log("Form submitted:", form.id);
+        });
+    });
+
+    // Example: Button click handling
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            // Handle button click logic here
+            console.log("Button clicked:", button.id);
+        });
+    });
+
+    // Additional event listeners and logic can be added as needed
+});

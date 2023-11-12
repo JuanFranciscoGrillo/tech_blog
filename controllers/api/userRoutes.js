27515cbf8b476e447e3be1c36a8ad6c129fa234d
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-12 11:48:11 -0400

    Implemented CRUD operations in controllers/api for users, posts, and comments

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -41,21 +41,56 @@ router.post('/login', async (req, res) => {
             req.session.username = userData.username;
             req.session.loggedIn = true;
 
-            res.json({ user: userData, message: 'You are now logged in!' });
+            res.status(200).json({ user: userData, message: 'You are now logged in!' });
         });
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
 });
 
-// POST route for user logout
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
+// Read operation - Get all users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update operation - Update a user
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
         });
-    } else {
-        res.status(404).end();
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete operation - Delete a user
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 

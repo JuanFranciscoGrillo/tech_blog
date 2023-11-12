44032437ba6c595a64aedf7b4d00d73def410367
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-12 12:27:13 -0400

    Extend helpers.js with additional utility functions for formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,19 @@
 module.exports = {
     format_date: (date) => {
+        // Format the date to MM/DD/YYYY
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
+    format_plural: (word, amount) => {
+        // Add 's' to the word if the amount is greater than 1
+        return amount !== 1 ? `${word}s` : word;
+    },
+    format_url: (url) => {
+        // Simplify the URL for display
+        return url
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
+    }
 };
